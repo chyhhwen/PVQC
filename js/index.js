2f89bbf525b599178cb5da0f99fc7395ed4b4532
chyhhwen <chyhhwen@gmail.com>
2024-11-11 11:37:18 +0800

    v1.0

--- js/index.js
+++ js/index.js
@@ -1,8 +1,18 @@
+var requestURL = './lib/data.json';
+var request = new XMLHttpRequest();
+var chinese = [];
 
-
-function getCookie(name) {
+function getCookie(name)
+{
     let match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
     return match ? match[2] : null;
+}
+
+getran = (min, max) =>
+{
+    min = Math.ceil(min);
+    max = Math.floor(max + 1);
+    return Math.floor(Math.random() * (max - min) + min);
 }
 
 reset = () =>
@@ -22,13 +32,37 @@ reset = () =>
     window.location.reload();
 }
 
+text = (temp) =>
+{
+    let quest = document.querySelector('#questions');
+    var put = "<span>" + temp + "</span>";
+    quest.innerHTML=put;
+}
+
+let data;
+
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function()
+{
+    data = request.response.data
+}
+
 window.onload = () =>
 {
     if(getCookie('name') == null)
     {
         document.cookie = "name=123";
     }
-    let data = getCookie('name');
-    //document.querySelector('#test').innerHTML = data;
+
+    let now = getran(0,data.length - 1);
+    /*if(now == temp)
+    {
+        now = getran(0,100);
+    }
+    temp = now;*/
+    text(data[now][0]);
+    //window.location.reload();
 }
 
